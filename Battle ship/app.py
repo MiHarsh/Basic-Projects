75ce72e7d4b136d7fe657de33778c2d4f7b11108
swaraj-jain <happy2000jain@gmail.com>
2022-10-26 11:07:06 +0530

    Game code done

--- Battle ship/app.py
+++ Battle ship/app.py
@@ -30,3 +30,46 @@ def get_ship_location():
         print("Please enter a valid column")
         column=input('Please enter a ship column A-H')
     return int(row)-1,let_to_num[column]
+
+
+#Function that creates the ships
+def create_ships(board):
+    for ship in range(5):
+        ship_r, ship_cl=randint(0,7), randint(0,7)
+        while board[ship_r][ship_cl] =='X':
+            ship_r, ship_cl = randint(0, 7), randint(0, 7)
+        board[ship_r][ship_cl] = 'X'
+
+def count_hit_ships(board):
+    count=0
+    for row in board:
+        for column in row:
+            if column=='X':
+                count+=1
+    return count
+
+
+create_ships(Hidden_Pattern)
+#print_board(Hidden_Pattern)
+turns = 10
+while turns > 0:
+    print('Welcome to Battleship')
+    print_board(Guess_Pattern)
+    row,column =get_ship_location()
+    if Guess_Pattern[row][column] == '-':
+        print(' You already guessed that')
+    elif Hidden_Pattern[row][column] =='X':
+        print(' Congratulations you have hit the battleship')
+        Guess_Pattern[row][column] = 'X'
+        turns -= 1
+    else:
+        print('Sorry,You missed')
+        Guess_Pattern[row][column] = '-'
+        turns -= 1
+    if  count_hit_ships(Guess_Pattern) == 5:
+        print("Congratulations you have sunk all the battleships")
+        break
+    print(' You have ' +str(turns) + 'turns remaining')
+    if turns == 0:
+        print('Game Over')
+        break
