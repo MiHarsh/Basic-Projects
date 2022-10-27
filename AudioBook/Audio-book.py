e843e338bcc434b9ccbd0b22a67b2366982919b9
swaraj-jain <happy2000jain@gmail.com>
2022-10-27 12:25:37 +0530

    modified audio book

--- AudioBook/Audio-book.py
+++ AudioBook/Audio-book.py
@@ -1,35 +1,25 @@
-#Importing Libraries
 #Importing Google Text to Speech library
 from gtts import gTTS
-
-#Importing PDF reader PyPDF2
 import PyPDF2
-
-#Open file Path
-pdf_File = open('name.pdf', 'rb') 
+File = open('file.pdf', 'rb') 
 
 #Create PDF Reader Object
-pdf_Reader = PyPDF2.PdfFileReader(pdf_File)
-count = pdf_Reader.numPages # counts number of pages in pdf
+pdf_R = PyPDF2.PdfFileReader(File)
+# counts number of pages in pdf
+pages = pdf_R.numPages 
 textList = []
 
 #Extracting text data from each page of the pdf file
-for i in range(count):
+for i in range(0,pages):
    try:
-    page = pdf_Reader.getPage(i)    
+    page = pdf_R.getPage(i)    
     textList.append(page.extractText())
    except:
        pass
 
 #Converting multiline text to single line text
 textString = " ".join(textList)
-
-print(textString)
-
-#Set language to english (en)
 language = 'en'
-
-#Call GTTS
 myAudio = gTTS(text=textString, lang=language, slow=False)
 
 #Save as mp3 file
